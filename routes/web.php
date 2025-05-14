dc5e2e1b3df14c258896b1ee500f992bd11b972b
EmmanuelZeng <emmazeng38@gmail.com>
2025-05-14 06:19:46 +0200

    feat: addd routes principals

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return inertia('Home');
 });
+Route::get('/about', function() {
+    return inertia('About');
+});
+Route::get('/cars', function() {
+    return inertia('Cars');
+});
+Route::get('/contact', function() {
+    return inertia('Contact');
+});
