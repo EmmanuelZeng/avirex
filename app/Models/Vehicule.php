7609fd24bd438a0062105c2d13b4ea8d9a371bb5
EmmanuelZeng <emmazeng38@gmail.com>
2025-05-22 15:41:59 +0200

    feat:add modele and type relationship

---
+++ app/Models/Vehicule.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Modele;
+use App\Models\TypeVehicule;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Vehicule extends Model
+{
+    protected $fillable = [
+        'modele_id',
+        'type_id',
+        'manifacture_year', //Année de fabrication
+        'mileage', //Kilométrage
+        'price', // prix
+        'type_carburant', //Type carburant
+        'transmission', //Transmission
+        'description', //description
+        'climatisation', //Climatisation
+        'gps', //GPS
+        'sieges_cuir', //Sièges en cuir
+        'sieges_chaufants', //Sièges chauffants
+        'toit_ouvrant',
+        'bluetooth',
+        'camera_recul',
+        'availability', //disponiblité
+    ];
+
+    public function modele(): BelongsTo
+    {
+        return $this->belongsTo(Modele::class);
+    }
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(TypeVehicule::class);
+    }
+}
