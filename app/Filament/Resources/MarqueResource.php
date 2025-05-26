47f3a69f12e50917eccd2c394d7b2a32901cf405
EmmanuelZeng <emmazeng38@gmail.com>
2025-05-26 23:26:04 +0200

    feat:add icon and navigation name

--- app/Filament/Resources/MarqueResource.php
+++ app/Filament/Resources/MarqueResource.php
@@ -15,15 +15,17 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class MarqueResource extends Resource
 {
+    protected static ?string $navigationLabel = 'Marque de voitures';
     protected static ?string $model = Marque::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-building-office';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                Forms\Components\TextInput::make('name')
+                    ->label('Nom de la Marque')
                     ->required()
             ]);
     }
@@ -32,7 +34,9 @@ class MarqueResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Noms de Marque')
+                    ->searchable(),
             ])
             ->filters([
                 //
